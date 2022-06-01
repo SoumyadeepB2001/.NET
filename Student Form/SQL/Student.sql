2963bcb34ee8afea8cfb48db37c120e00121fcca
SoumyadeepB2001 <soumyadeepbanerjee40@gmail.com>
2022-06-02 03:09:37 +0530

    Created stored procedure for insertion

--- Student Form/SQL/Student.sql
+++ Student Form/SQL/Student.sql
@@ -5,3 +5,13 @@ id INT IDENTITY(1,1),
 name VARCHAR (50) NOT NULL,
 roll INT PRIMARY KEY
 );
+
+CREATE PROC addName(
+@name NVARCHAR(50),
+@roll INT)
+AS
+BEGIN
+INSERT INTO student(name, roll) VALUES (@name, @roll)
+END
+
+exec addName @name="Toni Kroos", @roll=8;
